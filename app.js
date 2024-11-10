0c7bb4374039216cb3db4d6f2825109303c0a0bb
SaiemAziz <sayemazizchy@gmail.com>
2024-11-11 04:19:52 +0600

    root link updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import contestRoute from './routes/contest_routes.js'
 import voteRoute from './routes/vote_routes.js'
 import postRoute from './routes/post_routes.js'
 const app = Router()
-app.get("/", (req, res) => res.send("Flaunt Fashion backend root API"));
+app.get("/v1", (req, res) => res.send("Flaunt Fashion backend root API version: 1"));
 app.use('/v1/users', userRoute)
 app.use('/v1/auth', authRoute)
 app.use('/v1/contests', contestRoute)
